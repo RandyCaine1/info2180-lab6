9ae42579dc8d3d31efd2dbcf198ec0a596c33792
Nicoy Caine <nicoycaine1@gmail.com>
2017-11-01 18:02:51 +0000

    Exercise 4

--- request.php
+++ request.php
@@ -14,5 +14,38 @@ $definition = [
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+ -print "<p>" . $definition[$query] . "</p>"; 		 +<entries> 
+     <definition name="Definition" author="John"> 
+         A statement of the exact meaning of a word, especially in a dictionary. 
+     </definition> 
+     <definition name="Bar" author="Voltaire"> 
+         A place that sells alcholic beverages.
+     </definition> 
+     <definition name="AJAX" author="Nietzsche"> 
+         Technique which involves the use of javascript and xml (or JSON).
+     </definition> 
+     <definition name="HTML" author="Alexander">
+         The standard markup language for creating web pages and web applications.
+     </definition>
+     <definition name="CSS" author="Socrates">
+         A style sheet language used for describing the presentation of a document written in a markup language.
+     </definition>
+     <definition name="JavaScript" author="Aristole">
+         A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+     </definition>
+     <definition name="PHP" author="Plato">
+         A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+     </definition>
+ </entries>';
+ 
+ if ($query == ""){
+     header('Content-Type: text/xml');
+     $xmlOutput = new SimpleXMLElement($xmldata);
+     echo $xmlOutput->asXML();
+ }
+ 
+ else{
+     print "<h3>" . strtoupper($query) . "</h3>";
+     print "<p>" . $definition[$query] . "</p>";
+ } 
--- search.js
+++ search.js
@@ -1,6 +1,9 @@
 
 
 $(document).ready(function(){
+    
+    //Individual searches
+    
      $("#search").on('click', function(){
         var query = $("#query").val();
         var link = "https://info2180-lab6-nickcaine.c9users.io/request.php?q="+query;
@@ -9,6 +12,31 @@ $(document).ready(function(){
             method: 'GET',
         }).done(function(res){
              $("#result").html(res);
+        }).fail(function(err){
+            $("#result").html(err);
+        });
+     });
+     
+     
+         //search all button
+     $("#searchall").on('click', function(){
+         var link = "https://info2180-lab6-nickcaine.c9users.io/request.php?q=&all=true";
+         
+         $.ajax(link, {
+            method: 'GET',
+            dataType: "xml",
+        }).done(function(res){
+            var defs = $(res).find('definition');
+            
+            $("#result").html(""); //clear if previous result given
+            $("#result").append('<ol></ol>');
+            
+            $(defs).each(function(){
+                var ret = '<h3>'+$(this).attr('name')+'</h3>';
+                ret += '<p>'+$(this).text()+'</p>';
+                ret += '<p> -'+$(this).attr('author')+'</p>';
+                $("#result ol").append('<li>'+ret+'</li>');
+            });
         }).fail(function(err){
             $("#result").html(err);
         });
